55bbeaee2fa254b87d69272ad40fc6515baa93fa
pixel <pixel.world7@mail.ru>
2022-03-11 23:03:39 +0300

    stylization component WideScaleSlider

--- client/src/app/components/common/wideScaleSlider.jsx
+++ client/src/app/components/common/wideScaleSlider.jsx
@@ -1,15 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import WideScaleSliderSlide from "../ui/wideScaleSliderSlide"
 
 const WideScaleSlider = ({ classWrap, title, dataSliders, pathImages }) => {
+	const [widthColumn, setWidthColumn] = useState(0)
+	const handlerArrowController = (direction) => {
+		console.log(direction)
+	}
+	const handlerRef = (widthColumn) => setWidthColumn(widthColumn)
 	return (
 		<div className={`${classWrap}__scale-wide-slider wide-scale-slider`}>
 			<h2 className="wide-scale-slider__title">{title}</h2>
 			<div className="wide-scale-slider__wrapper">
 				<div className="wide-scale-slider__body">
-					{dataSliders.map((item, index) => <WideScaleSliderSlide {...item} key={index} pathImages={pathImages} />)}
+					{dataSliders.map((item, index) => <WideScaleSliderSlide id={index} onHandlerRef={handlerRef} {...item} key={index} pathImages={pathImages} />)}
 				</div>
+				<button onClick={() => {
+					handlerArrowController("left")
+				}} type="button" className="wide-scale-slider__btn-prev control-wide-scale-slider">
+					<img src="./images/icons/arrow-triangle-white.svg" alt="Иконка треугольной трелки" />
+				</button>
+				<button onClick={() => {
+					handlerArrowController("right")
+				}} type="button" className="wide-scale-slider__btn-next control-wide-scale-slider">
+					<img src="./images/icons/arrow-triangle-white.svg" alt="Иконка треугольной трелки" />
+				</button>
 			</div>
 		</div>
 	)
--- client/src/app/components/ui/wideScaleSliderSlide.jsx
+++ client/src/app/components/ui/wideScaleSliderSlide.jsx
@@ -1,18 +1,30 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
 import PropTypes from "prop-types"
 
-const WideScaleSliderSlide = ({ pathImages, slider1, title, slider2, slider2Title }) => {
+const WideScaleSliderSlide = ({ id, pathImages, slider1, title, slider2, slider2Title, onHandlerRef }) => {
+	const refColumn = useRef(null)
+	useEffect(() => {
+		if (id === 0) {
+			onHandlerRef(refColumn.current.offsetWidth)
+		}
+	}, [id, onHandlerRef])
 	return (
 		<>
-			<div className="wide-scale-slider__column">
+			<div ref={refColumn} className="wide-scale-slider__column wide-scale-slider__column_first">
 				<img src={pathImages + slider1.path} alt={slider1.alt} />
 				<div className="wide-scale-slider__content">
 					<h3 className="wide-scale-slider__text">{title}</h3>
-					<button className="wide-scale-slider__btn">Подробнее</button>
+					<button type="button" className="wide-scale-slider__btn">Подробнее</button>
 				</div>
 			</div>
 			<div className="wide-scale-slider__column wide-scale-slider__column_additional">
-				{slider2.map((el, index) => <img key={index} src={pathImages + el.path} alt={el.alt} />)}
+				{slider2.map((el, index) => {
+					return (
+						<div key={index} className="wide-scale-slider__block-add">
+							<img src={pathImages + el.path} alt={el.alt} />
+						</div>
+					)
+				})}
 				<div className="wide-scale-slider__name">{slider2Title}</div>
 			</div>
 		</>
@@ -24,7 +36,9 @@ WideScaleSliderSlide.propTypes = {
 	slider1: PropTypes.object.isRequired,
 	title: PropTypes.string.isRequired,
 	slider2: PropTypes.array.isRequired,
-	slider2Title: PropTypes.string.isRequired
+	slider2Title: PropTypes.string.isRequired,
+	onHandlerRef: PropTypes.func.isRequired,
+	id: PropTypes.number.isRequired
 }
 
 export default WideScaleSliderSlide
