d508edb3a9469fbee75d6086c0a3c7a6245b1267
VladislavPixel <pixel.world7@mail.ru>
2022-04-15 15:34:44 +0300

    create logic comments-slider for store

--- client/src/app/components/ui/commentCardSliderComments.jsx
+++ client/src/app/components/ui/commentCardSliderComments.jsx
@@ -1,14 +1,32 @@
-import React from "react"
+import React, { useState, useRef, useEffect } from "react"
 import PropTypes from "prop-types"
 
 // Components
 import SlideCommentCardSliderComments from "./slideCommentCardSliderComments"
 
 const CommentCardSliderComments = ({ dataArray }) => {
+	// STATE
+	const [currentComment, setCurrentComment] = useState(0)
+	const [widthWrapper, setWidthWrapper] = useState(null)
+	const [stylesBlockComments, setStylesBlockComments] = useState({ transform: `translateX(0px)` })
+	// Auxiliary
+	const wrapperBlock = useRef(null)
+	// Handlers
+	const handlerUpdateCurrentComment = (directionTxt) => {
+		if (directionTxt === "left" && currentComment !== 0) setCurrentComment(prevState => prevState - 1)
+		if (directionTxt === "right" && currentComment !== dataArray.length - 1) setCurrentComment(prevState => prevState + 1)
+	}
+
+	useEffect(() => {
+		setWidthWrapper(wrapperBlock.current.offsetWidth)
+	}, [])
+	useEffect(() => {
+		setStylesBlockComments({ transform: `translateX(-${currentComment * widthWrapper}px)` })
+	}, [currentComment, widthWrapper])
 	return (
 		<div className="comments-slider-block__comment-item item-comment-comments-slider">
-			<div className="item-comment-comments-slider__wrapper">
-				<div className="item-comment-comments-slider__sub-comments-block block-comments-sub">
+			<div ref={wrapperBlock} className="item-comment-comments-slider__wrapper">
+				<div style={stylesBlockComments} className="item-comment-comments-slider__sub-comments-block block-comments-sub">
 					{dataArray.length > 0 ? 
 						dataArray.map(comment => <SlideCommentCardSliderComments key={comment._id} {...comment} />):
 						<div className="item-comment-comments-slider__info">
@@ -17,7 +35,13 @@ const CommentCardSliderComments = ({ dataArray }) => {
 						</div>
 					}
 				</div>
-				Контроллер
+				{dataArray.length > 0 &&
+					<div className="item-comment-comments-slider__controller-comments comments-controller">
+						<button onClick={() => handlerUpdateCurrentComment("left")} className={"comments-controller__prev btn-comments-controller" + (currentComment === 0 ? " active" : "")}></button>
+						<p className="comments-controller__info-page">Обзоров: {`${currentComment + 1} из ${dataArray.length}`}</p>
+						<button onClick={() => handlerUpdateCurrentComment("right")} className={"comments-controller__next btn-comments-controller" + ((dataArray.length - 1) === currentComment ? " active" : "")}></button>
+					</div>
+				}
 			</div>
 		</div>
 	)
--- client/src/app/components/ui/slideCommentCardSliderComments.jsx
+++ client/src/app/components/ui/slideCommentCardSliderComments.jsx
@@ -6,11 +6,21 @@ import { useSelector } from "react-redux"
 import { getDataUserForCommentById } from "../../store/commentsGamesStoreSlider"
 
 const SlideCommentCardSliderComments = ({ userId, date, text }) => {
-	const userForSlide = useSelector(getDataUserForCommentById(userId))
-	console.log(userForSlide)
+	const { avatar, nickName } = useSelector(getDataUserForCommentById(userId))
 	return (
 		<div className="block-comments-sub__slide">
-
+			<div className="block-comments-sub__content-wrap comment-content-wrap">
+				<p className="comment-content-wrap__date">Дата комментария: {date}</p>
+				<p className="comment-content-wrap__text-comment">{`"${text}"`}</p>
+				<div className="comment-content-wrap__profile profile-comment">
+					<div className="profile-comment__column">
+						<img src={`./images/users/avatar/${avatar}`} alt={`Аватарка профайла - ${nickName}`} />
+					</div>
+					<div className="profile-comment__column">
+						<p className="profile-comment__nick">{nickName}</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
